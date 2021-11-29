fc3f4e5e318cc9ef7b942a4ef8ec36db3c559adc
RospirkoNatalia <78441769+RospirkoNatalia@users.noreply.github.com>
2021-11-29 11:22:37 +0200

    task 2.3 done

---
+++ task_2/app.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector('button');
+
+const params = [
+  {
+    color: '#18D158',
+    text: 'Update',
+  },
+  {
+    color: '#CD1D1D',
+    text: 'Delete',
+  },
+  {
+    color: '#E18523',
+    text: 'Pendding',
+  }
+];
+
+let current = 0;
+change ();
+
+function change () {
+  btn.textContent = params[current].text;
+  btn.style.background = params[current].color;
+}
+
+
+btn.addEventListener('click', () => {
+  if(current == 3) current = 0;
+  change ();
+  current++;
+});
